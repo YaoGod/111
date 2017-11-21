964b74ff6a19579a22c234866b25cc4dc3db67eb
zhangyaoyao_ext <zhangyaoyao@oneapm.com>
2017-11-21 16:14:37 +0800

    commit

--- src/app/service/repair-building/repair-building.service.ts
+++ src/app/service/repair-building/repair-building.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, RequestOptions, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 @Injectable()
-export class InfoBuildingService {
+export class RepairBuildingService {
 
   private headers = new Headers({'Content-Type': 'application/json'});
   private options =  new RequestOptions({
