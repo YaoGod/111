fec1f82a38430821b99f4b8c2e2a388202d5d257
YaoGod <1023612402@qq.com>
2018-05-07 08:51:41 +0800

    工作流动态目录列表（缺失文件）

---
+++ src/app/system-setting/flow/flow-config/flow-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flow-config',
+  templateUrl: './flow-config.component.html',
+  styleUrls: ['./flow-config.component.css']
+})
+export class FlowConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/system-setting/flow/job-mang/job-mang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-mang',
+  templateUrl: './job-mang.component.html',
+  styleUrls: ['./job-mang.component.css']
+})
+export class JobMangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
