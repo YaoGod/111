2f89443a987343dde8768c1e90aa3777d291c23e
gaomin_ext <gaomin@oneapm.com>
2018-05-18 08:32:05 +0800

    11

--- src/app/service/ip-setting/ip-setting.service.ts
+++ src/app/service/ip-setting/ip-setting.service.ts
@@ -12,11 +12,11 @@ export class IpSettingService {
   /* nginx */
   // public ip = "/proxy";
   // public ip = "http://hzzh.zj.chinamobile.com/hzbs";
-  // public ip = "http://localhost:8080";
+   public ip = "http://localhost:8080";
   /*set ip and port for personal*/
   // public ip = "http://20.26.28.6:8081";
   // public ip = "http://10.71.246.81:8080";
-  public ip = "http://10.71.246.83:8080";
+  //public ip = "http://10.71.246.83:8080";
   // public ip = "http://20.26.28.4:8080";
  constructor(
     private http: Http,
